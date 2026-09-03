0716cbbf86f39823f5b754b223c7014353fc18e7
sthamarai524-dot <sthamarai524@gmail.com>
2026-09-03 12:12:15 +0530

    Update solution.sql

--- Assignment10_Left_Right_Join/solution.sql
+++ Assignment10_Left_Right_Join/solution.sql
@@ -11,3 +11,39 @@ USE CollegeDB;
 -- LEFT JOIN
 
 -- RIGHT JOIN
+CREATE TABLE Course (
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(100)
+);
+
+INSERT INTO Course (CourseID, CourseName) VALUES
+    (101, 'Operating Systems'),
+    (102, 'Computer Networks'),
+    (103, 'Database Management');
+
+CREATE TABLE Enrollment (
+    EnrollmentID INT PRIMARY KEY,
+    StudentName VARCHAR(100),
+    CourseID INT
+);
+
+INSERT INTO Enrollment (EnrollmentID, StudentName, CourseID) VALUES
+    (1, 'Vikram', 101),
+    (2, 'Ananya', 101),
+    (3, 'Surya', 102),
+    (4, 'Meera', NULL);
+
+SELECT 
+    Enrollment.StudentName, 
+    Course.CourseName
+FROM 
+    Enrollment
+LEFT JOIN 
+    Course ON Enrollment.CourseID = Course.CourseID;
+SELECT 
+    Enrollment.StudentName, 
+    Course.CourseName
+FROM 
+    Enrollment
+RIGHT JOIN 
+    Course ON Enrollment.CourseID = Course.CourseID;

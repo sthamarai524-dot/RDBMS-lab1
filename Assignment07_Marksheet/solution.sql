40d6cf87282293d6cb7d11412c2f7d98289483d5
sthamarai524-dot <sthamarai524@gmail.com>
2026-09-03 12:08:01 +0530

    Update solution.sql

--- Assignment07_Marksheet/solution.sql
+++ Assignment07_Marksheet/solution.sql
@@ -8,3 +8,21 @@ USE CollegeDB;
 -- Display students with Marks > 80
 
 -- Sort by Marks DESC
+CREATE TABLE Marksheet (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(30),
+    Marks INT
+);
+
+INSERT INTO Marksheet (StudentID, StudentName, Marks) 
+VALUES 
+    (101, 'Arun', 85),
+    (102, 'Divya', 75),
+    (103, 'Karthik', 92),
+    (104, 'Priya', 88),
+    (105, 'Rahul', 60);
+
+SELECT * 
+FROM Marksheet 
+WHERE Marks > 80 
+ORDER BY Marks DESC;
